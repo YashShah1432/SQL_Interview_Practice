05c9d98d588adce2300f225f443fedadf2138322
Yash Shah <yashah1432@gmail.com>
2026-07-14 00:48:17 +0530

    Add solution for LeetCode SQL Problem 1084

---
+++ 1084-SQL-Solution/Solution.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select p.product_id, p.product_name from product p
+join sales s
+on p.product_id = s.product_id
+where s.sale_date between '2019-01-01' and '2019-03-31'
+and p.product_id not in (select product_id from sales where sale_date between '2019-04-01' and '2019-12-31' or year(sale_date) != 2019)
+group by p.product_id;
