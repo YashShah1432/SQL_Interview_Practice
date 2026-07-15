137e05b2bd64dff1d849cb66228bb1945e921081
Yash Shah <yashah1432@gmail.com>
2026-07-15 22:07:35 +0530

    Add solution for LeetCode SQL Problem 1587

---
+++ 1587-SQL-Solution/Solution.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select 
+    u.name, 
+    sum(t.amount) as balance 
+from users u
+join Transactions t
+on u.account = t.account
+group by u.account
+having sum(t.amount) > 10000;
